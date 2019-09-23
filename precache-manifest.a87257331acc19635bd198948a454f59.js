9f4a34e87c0d9ed9ce31dae5a33384ad3c30ed2d
Oliver Kwast <o.kwast@lokalportal.de>
2019-09-23 08:45:28 +0200

    Updates

---
+++ precache-manifest.a87257331acc19635bd198948a454f59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dae20e9c6290ecca9589002cbe5a9fb",
+    "url": "/PlanningPoker/index.html"
+  },
+  {
+    "revision": "f88500e64c3a787cff7e",
+    "url": "/PlanningPoker/static/css/main.0a75b6db.chunk.css"
+  },
+  {
+    "revision": "85104ad16241e2810204",
+    "url": "/PlanningPoker/static/js/2.854ac6cd.chunk.js"
+  },
+  {
+    "revision": "f88500e64c3a787cff7e",
+    "url": "/PlanningPoker/static/js/main.a856627c.chunk.js"
+  },
+  {
+    "revision": "f23f18132f6ba0a96efe",
+    "url": "/PlanningPoker/static/js/runtime-main.eac19345.js"
+  }
+]);
